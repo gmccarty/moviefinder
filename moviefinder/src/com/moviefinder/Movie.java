cf4b26af3ff2f73966518d62a088450819701db1
Grant McCarty <grant.e.mcc@gmail.com>
2019-07-20 13:58:49 -0400

    Added classes and method stubs

---
+++ moviefinder/src/com/moviefinder/Movie.java
@@ -0,0 +1,20 @@
+package com.moviefinder;
+
+public class Movie {
+	
+	private String title, year, audScore, rtUrl, imdbUrl;
+	private String[] userTags;
+	
+	//Constructor for Query 1 
+	public Movie(String title, String year, String audScore, String rtUrl, String imdbUrl) {
+		this.title = title;
+		this.year = year;
+		this.audScore = audScore;
+		this.rtUrl = rtUrl;
+		this.imdbUrl = imdbUrl;
+	}
+	
+	
+	
+	
+}
---
+++ moviefinder/src/com/moviefinder/gui/GUIDriver.java
@@ -0,0 +1,7 @@
+package com.moviefinder.gui;
+
+public class GUIDriver {
+	
+	
+	
+}
