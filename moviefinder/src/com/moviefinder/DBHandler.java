be295aafc5217767221b9fe4a18afd4a4800f84a
Grant McCarty <grant.e.mcc@gmail.com>
2019-07-23 18:37:35 -0400

    Cleaned up some code and created helper methods

--- moviefinder/src/com/moviefinder/DBHandler.java
+++ moviefinder/src/com/moviefinder/DBHandler.java
@@ -1,288 +1,237 @@
 package com.moviefinder;
-import java.sql.*;
-import java.util.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class DBHandler {
+
+	private Connection connection;
 	
+	public DBHandler() {
+		connect();
+	}
 	
-	@SuppressWarnings("finally")
-	public ArrayList<Movie> query1(int k) {
-		Statement stmt = null;
-		try {
-			stmt = databaseConnector().createStatement();
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			
-		}
+	public ArrayList<Movie> query1(int k) throws SQLException {
+		Statement stmt = getStatement();
 		String sql = "SELECT title, year, aud_score, rt_url, imdb_url " +
-					 "FROM movies " +
-					 "ORDER BY aud_score DESC" +
-					 "LIMIT " + k;
-		ArrayList<Movie> movies = new ArrayList<>();
-		try {
-			ResultSet rs = stmt.executeQuery(sql);
-			while(rs.next()) {
-				String name = rs.getString("title");
-				int year = rs.getInt("year");
-				int aud_score = rs.getInt("aud_score");
-				String rt_url = rs.getString("rt_url");
-				String imdb_url = rs.getString("imdb_url");
-				Movie m = new Movie(name, year, aud_score, rt_url, imdb_url);
-				movies.add(m);
-			}
-			rs.close();
-			stmt.close();
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			return movies;
-		}
-		
-		
-	}
-	
-	public ArrayList<Movie> query2(String title) {
-		Statement stmt = null;
-		try {
-			stmt = databaseConnector().createStatement();
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			
-		}
-		
-		
-		
-		return null;
-	}
-	
-	@SuppressWarnings("finally")
-	public ArrayList<Movie> query3(String genre, int k) {
-		Statement stmt = null;
-		try {
-			stmt = databaseConnector().createStatement();
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			
-		}
-		String sql = "SELECT m.title, m.year, m.aud_score, m.rt_url, m.imdb_url " +
-				 	 "FROM movies m, movie_genres mg " +
-				 	 "WHERE m.movie_id = mg.movie_id AND mg.genre LIKE" + genre + " " +
-				 	 "ORDER BY aud_score DESC " +
-				 	 "LIMIT " + k;
-		ArrayList<Movie> movies = new ArrayList<>();
-		try {
-			ResultSet rs = stmt.executeQuery(sql);
-			while(rs.next()) {
-				String name = rs.getString("title");
-				int year = rs.getInt("year");
-				int aud_score = rs.getInt("aud_score");
-				String rt_url = rs.getString("rt_url");
-				String imdb_url = rs.getString("imdb_url");
-				Movie m = new Movie(name, year, aud_score, rt_url, imdb_url);
-				movies.add(m);
-			}
-			rs.close();
-			stmt.close();
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			return movies;
-		}
+				"FROM movies " +
+				"ORDER BY aud_score DESC" +
+				"LIMIT " + k;
 
-	}
-	
-	@SuppressWarnings("finally")
-	public ArrayList<Movie> query4(String director) {
-		Statement stmt = null;
-		try {
-			stmt = databaseConnector().createStatement();
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			
-		}
-		String sql = "SELECT m.title, m.year, m.aud_score, m.rt_url, m.imdb_url " +
-					 "FROM movies m, movie_directors md " +
-					 "WHERE m.movie_id = md.movie_id AND md.director_name LIKE " + director;
 		ArrayList<Movie> movies = new ArrayList<>();
-		try {
-			ResultSet rs = stmt.executeQuery(sql);
-			while(rs.next()) {
-				String name = rs.getString("title");
-				int year = rs.getInt("year");
-				int aud_score = rs.getInt("aud_score");
-				String rt_url = rs.getString("rt_url");
-				String imdb_url = rs.getString("imdb_url");
-				Movie m = new Movie(name, year, aud_score, rt_url, imdb_url);
-				movies.add(m);
-			}
-			rs.close();
-			stmt.close();
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			return movies;
-		}
-		
-	}
-	
-	public ArrayList<Movie> query5(String actor) {
-		Statement stmt = null;
-		try {
-			stmt = databaseConnector().createStatement();
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			
-		}
-		String sql = "SELECT m.title, m.year, m.aud_score, m.rt_url, m.imdb_url " +
-				 "FROM movies m, movie_actors ma " +
-				 "WHERE m.movie_id = ma.movie_id AND ma.actor_name LIKE " + actor;
-		ArrayList<Movie> movies = new ArrayList<>();
-		try {
-			ResultSet rs = stmt.executeQuery(sql);
-			while(rs.next()) {
-				String name = rs.getString("title");
-				int year = rs.getInt("year");
-				int aud_score = rs.getInt("aud_score");
-				String rt_url = rs.getString("rt_url");
-				String imdb_url = rs.getString("imdb_url");
-				Movie m = new Movie(name, year, aud_score, rt_url, imdb_url);
-				movies.add(m);
-			}
-			rs.close();
-			stmt.close();
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			return movies;
-		}
-		
-		
 
+		ResultSet rs = stmt.executeQuery(sql);
+		while(rs.next()) {
+			String name = rs.getString("title");
+			int year = rs.getInt("year");
+			int aud_score = rs.getInt("aud_score");
+			String rt_url = rs.getString("rt_url");
+			String imdb_url = rs.getString("imdb_url");
+			Movie m = new Movie(name, year, aud_score, rt_url, imdb_url);
+			movies.add(m);
+		}
+		
+		close(stmt, rs);
+		return movies;
+	}
+
+	public ArrayList<Movie> query2(String title) throws SQLException {
+		Statement stmt = getStatement();
+		String sql = ""; 										//TODO: write query
+		
+		ArrayList<Movie> movies = new ArrayList<>();
+		
+		ResultSet rs = stmt.executeQuery(sql);
+		while(rs.next()) {
+																//TODO: extract data
+		}
+		
+		close(stmt, rs);
+		return movies;
 	}
 	
-	public ArrayList<Movie> query6(String tag) {
-		Statement stmt = null;
-		try {
-			stmt = databaseConnector().createStatement();
+	public ArrayList<Movie> query3(String genre, int k) throws SQLException {
+		Statement stmt = getStatement();
+		String sql = "SELECT m.title, m.year, m.aud_score, m.rt_url, m.imdb_url " +
+				"FROM movies m, movie_genres mg " +
+				"WHERE m.movie_id = mg.movie_id AND mg.genre LIKE" + genre + " " +
+				"ORDER BY aud_score DESC " +
+				"LIMIT " + k;
+		
+		ArrayList<Movie> movies = new ArrayList<>();
+		ResultSet rs = stmt.executeQuery(sql);
+		while(rs.next()) {
+			String name = rs.getString("title");
+			int year = rs.getInt("year");
+			int aud_score = rs.getInt("aud_score");
+			String rt_url = rs.getString("rt_url");
+			String imdb_url = rs.getString("imdb_url");
+			Movie m = new Movie(name, year, aud_score, rt_url, imdb_url);
+			movies.add(m);
 		}
-		catch (SQLException e) {
+		
+		close(stmt, rs);
+		return movies;
+	}
+	
+	public ArrayList<Movie> query4(String director) throws SQLException {
+		Statement stmt = getStatement();
+		String sql = "SELECT m.title, m.year, m.aud_score, m.rt_url, m.imdb_url " +
+				"FROM movies m, movie_directors md " +
+				"WHERE m.movie_id = md.movie_id AND md.director_name LIKE " + director;
+		
+		ArrayList<Movie> movies = new ArrayList<>();
+		
+		ResultSet rs = stmt.executeQuery(sql);
+		while(rs.next()) {
+			String name = rs.getString("title");
+			int year = rs.getInt("year");
+			int aud_score = rs.getInt("aud_score");
+			String rt_url = rs.getString("rt_url");
+			String imdb_url = rs.getString("imdb_url");
+			Movie m = new Movie(name, year, aud_score, rt_url, imdb_url);
+			movies.add(m);
+		}
+		
+		close(stmt, rs);
+		return movies;
+	}
+
+	public ArrayList<Movie> query5(String actor) throws SQLException {
+		Statement stmt = getStatement();
+		String sql = "SELECT m.title, m.year, m.aud_score, m.rt_url, m.imdb_url " +
+				"FROM movies m, movie_actors ma " +
+				"WHERE m.movie_id = ma.movie_id AND ma.actor_name LIKE " + actor;
+		
+		ArrayList<Movie> movies = new ArrayList<>();
+		
+		ResultSet rs = stmt.executeQuery(sql);
+		while(rs.next()) {
+			String name = rs.getString("title");
+			int year = rs.getInt("year");
+			int aud_score = rs.getInt("aud_score");
+			String rt_url = rs.getString("rt_url");
+			String imdb_url = rs.getString("imdb_url");
+			Movie m = new Movie(name, year, aud_score, rt_url, imdb_url);
+			movies.add(m);
+		}
+		
+		close(stmt, rs);
+		return movies;
+	}
+
+	public ArrayList<Movie> query6(String tag) throws SQLException {
+		Statement stmt = getStatement();
+		String sql = ""; 										//TODO: write query
+		
+		ArrayList<Movie> movies = new ArrayList<>();
+		
+		ResultSet rs = stmt.executeQuery(sql);
+		while(rs.next()) {
+																//TODO: extract data
+		}
+		
+		close(stmt, rs);
+		return movies;
+	}
+
+	public ArrayList<Movie> query7(int k) throws SQLException {
+		Statement stmt = getStatement();
+		String sql = ""; 										//TODO: write query
+		
+		ArrayList<Movie> movies = new ArrayList<>();
+		
+		ResultSet rs = stmt.executeQuery(sql);
+		while(rs.next()) {
+																//TODO: extract data
+		}
+		
+		close(stmt, rs);
+		return movies;
+	}
+
+	public ArrayList<Movie> query8(int k) throws SQLException {
+		Statement stmt = getStatement();
+		String sql = ""; 										//TODO: write query
+		
+		ArrayList<Movie> movies = new ArrayList<>();
+		
+		ResultSet rs = stmt.executeQuery(sql);
+		while(rs.next()) {
+																//TODO: extract data
+		}
+		
+		close(stmt, rs);
+		return movies;
+	}
+
+	public ArrayList<Movie> query9(int k) throws SQLException {
+		Statement stmt = getStatement();
+		String sql = ""; 										//TODO: write query
+		
+		ArrayList<Movie> movies = new ArrayList<>();
+		
+		ResultSet rs = stmt.executeQuery(sql);
+		while(rs.next()) {
+																//TODO: extract data
+		}
+		
+		close(stmt, rs);
+		return movies;
+	}
+
+	public ArrayList<Movie> query10(String title) throws SQLException {
+		Statement stmt = getStatement();
+		String sql = ""; 										//TODO: write query
+		
+		ArrayList<Movie> movies = new ArrayList<>();
+		
+		ResultSet rs = stmt.executeQuery(sql);
+		while(rs.next()) {
+																//TODO: extract data
+		}
+		
+		close(stmt, rs);
+		return movies;
+	}
+
+	
+	/* HELPERS */
+	
+	private void connect() {
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", "root", "De356fine");
+			if (connection != null)
+				System.out.println("Connected to the database.");
+			
+		} catch (SQLException e) {
+			System.out.println("An error occurred. Check user credentials.");
 			e.printStackTrace();
 		}
-		finally {
-			
+	}
+	
+	private Statement getStatement() {
+		try {
+			return connection.createStatement();
+		} catch (SQLException e) {
+			System.out.println("An error occurred while creating Statement.");
+			e.printStackTrace();
 		}
-		
-		
-		
 		return null;
 	}
 	
-	public ArrayList<Movie> query7(int k) {
-		Statement stmt = null;
+	private void close(Statement stmt, ResultSet rs) {
 		try {
-			stmt = databaseConnector().createStatement();
-		}
-		catch (SQLException e) {
+			stmt.close();
+			rs.close();
+		} catch (SQLException e) {
+			System.out.println("An error occurred while closing Statment and ResultSet.");
 			e.printStackTrace();
 		}
-		finally {
-			
-		}
-		
-		
-		
-		return null;
 	}
 	
-	public ArrayList<Movie> query8(int k) {
-		Statement stmt = null;
-		try {
-			stmt = databaseConnector().createStatement();
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			
-		}
-		
-		
-		
-		return null;
-	}
-	
-	public ArrayList<Movie> query9(int k) {
-		Statement stmt = null;
-		try {
-			stmt = databaseConnector().createStatement();
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			
-		}
-		
-		
-		
-		return null;
-	}
-	
-	public ArrayList<Movie> query10(String title) {
-		Statement stmt = null;
-		try {
-			stmt = databaseConnector().createStatement();
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			
-		}
-		
-		
-		
-		return null;
-	}
-	
-	@SuppressWarnings("finally")
-	public Connection databaseConnector() {
-		Connection con = null;
-		try {
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", "root", "De356fine");
-			if (con != null) {
-                System.out.println("Connected to the database");
-			}
-		}
-		catch(SQLException e){
-			System.out.println("An error occurred. Maybe user/password is invalid");
-            e.printStackTrace();
-		}
-		finally {
-           return con;
-        }
-	}
 }
--- moviefinder/src/com/moviefinder/Movie.java
+++ moviefinder/src/com/moviefinder/Movie.java
@@ -2,11 +2,11 @@ package com.moviefinder;
 
 public class Movie {
 	
-	private String title, rtUrl, imdbUrl;
 	private int year, audScore;
+	private String title, rtUrl, imdbUrl;
 	private String[] userTags;
 	
-	//Constructor for Query 1 
+	//Constructor - Query 1, 3, 4, 5
 	public Movie(String title, int year, int audScore, String rtUrl, String imdbUrl) {
 		this.title = title;
 		this.year = year;
@@ -15,7 +15,11 @@ public class Movie {
 		this.imdbUrl = imdbUrl;
 	}
 	
-	
-	
+	public int getYear() 			{ return year;		}
+	public int getAudScore()		{ return audScore;	}
+	public String getTitle() 		{ return title;		}
+	public String getRtUrl() 		{ return rtUrl;		}
+	public String getImdbUrl() 		{ return imdbUrl;	}
+	public String[] getUserTags() 	{ return userTags;	}
 	
 }
